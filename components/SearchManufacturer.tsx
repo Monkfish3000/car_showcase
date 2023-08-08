b6e559e57c84041dc1a543c1cbb3e9de8710ff81
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-08-08 11:26:55 +0200

    install headlessui

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+'use client';
+
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
+
 import { SearchManufacturerProps } from '@/types';
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="car-logo.svg"
+              alt="car logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
