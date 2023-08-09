7b11f7b05bbeba401584c4f3dc727a70de469d4d
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-08-09 09:45:17 +0200

    car card written and styled

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+'use client';
 
-const CarCard = ({ car }) => {
-  return <div>CarCard</div>;
+import Image from 'next/image';
+import { useState } from 'react';
+
+import { CarProps } from '@/types';
+import { CustomButton } from '.';
+import { calcCarRent } from '@/utils';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const carRentPrice = calcCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRentPrice}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          className="object-contain"
+          fill
+        />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="steering wheel icon"
+              width={20}
+              height={20}
+            />
+            <p className="p-[14px]">
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tyre.svg" alt="tyre icon" width={20} height={20} />
+            <p className="p-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/gas.svg"
+              alt="petrol pump icon"
+              width={20}
+              height={20}
+            />
+            <p className="p-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
