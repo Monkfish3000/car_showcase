c1a2c30776c9b8475ecac14c812d9d06ac5313ff
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-08-09 09:23:55 +0200

    catalogue added to home page

--- utils/index.ts
+++ utils/index.ts
@@ -4,9 +4,11 @@ export async function fetchCars() {
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
     { headers: headers }
   );
 
   const results = await response.json();
+
+  return results;
 }
