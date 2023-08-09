013f7c96b8ed657d9b9ebcf8ed60d13608158904
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-08-09 09:06:23 +0200

    rapid api cars function written for fetching cars

---
+++ utils/index.ts
@@ -0,0 +1,12 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': 'dc9db1b44cmshfe6839673e6ee04p15733djsn790a9ef56723',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    { headers: headers }
+  );
+
+  const results = await response.json();
+}
